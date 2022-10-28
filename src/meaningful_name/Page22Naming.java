a834918407c9879bbba8b57768657a3248ef4423
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-28 17:08:57 +0900

    Code with Naming Rule

---
+++ src/meaningful_name/Page22Naming.java
@@ -0,0 +1,26 @@
+package meaningful_name;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Page22Naming {
+    
+    final int STATUS_VALUE = 0;
+    final int FLAGGED = 1;
+    
+    public List<Cell> getFlaggedCells() {
+        List<Cell> flaggedCells = new ArrayList<>();
+        List<Cell> gameBoard = new ArrayList<>();
+
+        for(Cell cell : gameBoard)
+            if(cell.isFlagged())
+                flaggedCells.add(cell);
+        return flaggedCells;
+    }
+}
+
+class Cell {
+    public boolean isFlagged(){
+        return true;
+    }
+}
