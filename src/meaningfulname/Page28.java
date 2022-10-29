a089da462622abbbfa533b8747cc3317fa75f251
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-29 17:20:25 +0900

    Use name with easy pronunciation

---
+++ src/meaningfulname/Page28.java
@@ -0,0 +1,21 @@
+package meaningfulname;
+
+import java.util.Date;
+
+public class Page28 {
+    //아래의 코드를 비교해보기.
+    //어느 변수명이 더 직관적인가?
+}
+
+class DtaRcrd102 {
+    private Date genymdhms;
+    private Date modymdhms;
+    private final String pszqint = "102";
+
+}
+
+class Customer {
+    private Date generationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId= "102";
+}
