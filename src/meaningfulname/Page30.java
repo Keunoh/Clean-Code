f36e2e515374a837b387df8a891824c46bce52d7
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-30 18:39:54 +0900

    Don't use prefix

---
+++ src/meaningfulname/Page30.java
@@ -0,0 +1,22 @@
+package meaningfulname;
+
+public class Page30 {
+    //접두어는 옛날에 작성한 구닥다리 코드라는 징표가 되어버린다.
+    //ex m_ <- 이런식의 표기법
+}
+
+class Part1 {
+    private String m_dsc; //설명 문자열
+
+    void setName(String name) {
+        m_dsc = name;
+    }
+}
+
+class Part2 {
+    String description;
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+}
