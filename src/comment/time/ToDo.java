273464fa6953279736f1652ac201b6a1c553ef32
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-03 20:29:33 +0900

    feature: the comment 'TODO' alarms the work that can't be resolved at the moment

---
+++ src/comment/time/ToDo.java
@@ -0,0 +1,10 @@
+package comment.time;
+
+public class ToDo {
+
+    //TODO-MdM 현재 필요하지 않다.
+    //체크아웃 모델을 도입하면 함수가 필요 없다.
+    protected VersionInfo makeVersion() throws Exception {
+        return null;
+    }
+}
