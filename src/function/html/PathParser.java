7d88969d7e5acb96c517c31232ea523ed193b5de
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-03 20:14:01 +0900

    feature: the comment that meaning is clarity

--- src/function/html/PathParser.java
+++ src/function/html/PathParser.java
@@ -4,4 +4,8 @@ public class PathParser {
     public static String render(WikiPagePath pagePath){
         return "page path";
     }
+
+    public static WikiPagePath parse(String page) {
+        return new WikiPagePath(page);
+    };
 }
--- src/function/html/WikiPagePath.java
+++ src/function/html/WikiPagePath.java
@@ -2,4 +2,19 @@ package function.html;
 
 public class WikiPagePath {
     public String names;
+
+    public WikiPagePath() {
+    }
+
+    public WikiPagePath(String names) {
+        this.names = names;
+    }
+
+    public int compareTo(WikiPagePath wikiPagePath) {
+        if (wikiPagePath != null) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 }
